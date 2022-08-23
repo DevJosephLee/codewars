2f6508f5ae89612b833b2726618b77d0d432dfde
Joseph Lee <teddylou0215@gmail.com>
2022-08-23 03:05:15 +0000

    my solution

--- 5kyu/simplePigLatin.js
+++ 5kyu/simplePigLatin.js
@@ -3,3 +3,16 @@
 //   Examples
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
+function pigIt(str) {
+  var strSplit = str.split(' ');
+  var output = []
+  for (var i = 0; i < strSplit.length; i++) {
+    output.push(strSplit[i].slice(1) + strSplit[i][0] + 'ay')
+  }
+  if (output[output.length - 1] === '!ay') {
+    output[output.length - 1] = '!'
+  } else if (output[output.length - 1] === '?ay') {
+    output[output.length - 1] = '?'
+  }
+  return output.join(' ')
+}
