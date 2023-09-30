82e7aa85f788444c5e0f0f0904669160e5355c6c
Joseph Lee <teddylou0215@gmail.com>
2023-09-29 18:28:18 -0700

    solution

---
+++ lfz/exerciseTwo.py
@@ -0,0 +1,12 @@
+# 1. Write a function to create a dictionary of binary values of
+# decimal of 0 to 15
+# 2. The decimal number should be the key and the binary number
+# should be the value
+# Example Output: {"0";0000, "1":0001, "2":00100,.....}
+
+dictionary = {}
+
+for num in range(16):
+    dictionary[str(num)] = format(num, "04b")
+
+return dictionary
