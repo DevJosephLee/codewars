1beeda69f1589dd29a639c22ca8fa8ea4a97451c
Joseph Lee <teddylou0215@gmail.com>
2023-10-12 17:40:17 -0700

    solution

---
+++ 7kyu/from0ToN.py
@@ -0,0 +1,38 @@
+# Description:
+
+# We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+# Example:
+# Input:
+
+# > 6
+# Output:
+
+# 0+1+2+3+4+5+6 = 21
+
+# Input:
+
+# > -15
+# Output:
+
+# -15<0
+
+# Input:
+
+# > 0
+# Output:
+
+# 0=0
+
+def show_sequence(n):
+    total = 0
+    result = ''
+    for num in range(n + 1):
+        total += num
+        result += str(num) + '+'
+    if abs(n) != n:
+        return str(n) + '<0'
+    elif n == 0:
+        return '0=0'
+    else:
+        return result[:-1] + ' = ' + str(total)
